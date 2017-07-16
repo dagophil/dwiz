e723317a7af05fcf61a24d336c0657472bcf9332
Philip Schill <philip.schill@gmx.de>
2017-07-16 20:31:47 +0200

    Wrapped client main code with try-catch and added some logging output.

--- client/client_main.cpp
+++ client/client_main.cpp
@@ -1,17 +1,35 @@
 #include <common/dwiz_std.h>
 #include <client/client_main_window.h>
+#include <common/log/logging.h>
 #include <QApplication>
-
+#include <exception>
 
 int main(int argc, char* argv[])
 {
-    QApplication app(argc, argv);
-    dwiz::ClientMainWindow window;
-    QObject::connect(
-        &window, &dwiz::ClientMainWindow::closed,
-        qApp, &QApplication::quit
-    );
-    window.show();
-    int const return_value = app.exec();
-    return return_value;
+    using namespace dwiz;
+
+    loginfo << "Application start." << endlog;
+    int return_code = 1;
+    try
+    {
+        QApplication app(argc, argv);
+        ClientMainWindow window;
+        QObject::connect(
+            &window, &ClientMainWindow::closed,
+            qApp, &QApplication::quit
+        );
+        window.show();
+        return_code = app.exec();
+    }
+    catch (std::exception const& ex)
+    {
+        logerr << "Uncaught exception: \"" << ex.what() << "\"." << endlog;
+    }
+    catch (...)
+    {
+        logerr << "Uncaught unknown exception." << endlog;
+    }
+
+    loginfo << "Application end." << endlog;
+    return return_code;
 }
